503fed4362901682a503a2671e1cbe23adb9c38f
vadym-usk <vadym.usk@gmail.com>
2025-04-14 13:43:08 +0300

    fixed tests

--- tests/e2e/setup/setup.spec.ts
+++ tests/e2e/setup/setup.spec.ts
@@ -1,6 +1,6 @@
 import { test as setup } from '@playwright/test';
 import path from 'path';
-import { performLogin } from '../../src/utils/auth';
+import { performLogin } from '../../../src/utils/auth';
 
 setup('Login and save storage state', async ({ page }) => {
     await performLogin(page, process.env.HTTP_CREDENTIALS_USERNAME as string, process.env.HTTP_CREDENTIALS_PASSWORD as string);
