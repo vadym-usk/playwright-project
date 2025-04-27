a4022745cbfd69c75b3ab6ab73cdc4e61087002c
vadym-usk <vadym.usk@gmail.com>
2025-04-27 19:26:27 +0300

    Update positive.spec.ts

--- tests/e2e/auth/positive.spec.ts
+++ tests/e2e/auth/positive.spec.ts
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 import { HomePage } from '../../../src/pages/HomePage';
 import { RegistrationPopup } from '../../../src/pages/RegistrationPopup';
 import { GaragePage } from '../../../src/pages/GaragePage';
-//import { GaragePage } from '../../../src/pages/GaragePage';
 
 test.describe('Auth - Positive', () => {
     let homePage;
